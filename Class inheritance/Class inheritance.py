0a1b1724bec8eddafce22d3c09443d8eb6a8009e
Hoferson <122641259+Hoferson@users.noreply.github.com>
2023-01-21 22:23:31 +0200

    Class inheritance

--- Class inheritance/Class inheritance.py
+++ Class inheritance/Class inheritance.py
@@ -1,12 +1,3 @@
-class Group_limit_error(Exception):
-
-    def __init__ (self, message, limit):
-        self.message = message
-        self.limit = limit
-
-    def __str__ (self):
-        return f'{self.message}{self.limit}'
-
 class Person:
 
     def __init__ (self, name, surname, age):
@@ -35,21 +26,17 @@ class Teacher(Person):
 
 class Group:
 
-    def __init__ (self, title, limit):
+    def __init__ (self, title):
         self.title = title
         self.members = []
-        self.limit = limit
 
     def add_member (self, member):
-        if len(self.members) < self.limit:
-            self.members.append(member)
-        else:
-            raise Group_limit_error('Group limit reached: ', self.limit)
-        
+        self.members.append(member)
+
     def remove_member (self, member):
         if member in self.members:
             self.members.remove(member)
-            
+
     def search (self, surname):
         for i in self.members:
             if i.surname == surname:
@@ -61,11 +48,8 @@ class Group:
     def __str__ (self):
         result = f"Group {self.title}\n"
         result += '\n'.join(map(lambda item: str(item) ,self.members))
-        
+
         return result
-    
-
-
 
 
 student_1 = Student('Max', 'Maximov', 16)
@@ -82,27 +66,23 @@ student_11 = Student('Buratino', 'Wooden', 1)
 
 teacher = Teacher('Danil', 'Danilov', 30)
 
-group = Group('Python_Pro', 10)
+group = Group('Python_Pro')
 
-try:
-    group.add_member(teacher)
-    group.add_member(student_1)
-    group.add_member(student_2)
-    group.add_member(student_3)
-    group.add_member(student_4)
-    group.add_member(student_5)
-    group.add_member(student_6)
-    group.add_member(student_7)
-    group.add_member(student_8)
-    group.add_member(student_9)
-    group.add_member(student_10)
-    group.add_member(student_11)
-
-except Group_limit_error as error:
-    print(error)
-
-
+group.add_member(teacher)
+group.add_member(student_1)
+group.add_member(student_2)
+group.add_member(student_3)
+group.add_member(student_4)
+group.add_member(student_5)
+group.add_member(student_6)
+group.add_member(student_7)
+group.add_member(student_8)
+group.add_member(student_9)
+group.add_member(student_10)
+group.add_member(student_11)
+group.remove_member(student_11)
 
 print(group)
 print('\nsearch result: '+ str(group.search('Ronaldov')))
+
 
